42f7a1c1871abbcc5f9688153376ce84a494ddd7
sidnt <11618157+sidnt@users.noreply.github.com>
2021-02-25 15:44:04 +0530

    add 2 tests that would blow up if without @@sequential

--- src/test/scala/zlmdb/a5-mdbKvsSpec.scala
+++ src/test/scala/zlmdb/a5-mdbKvsSpec.scala
@@ -2,6 +2,7 @@ package zio.lmdb
 
 import zio.test._
 import zio.test.Assertion._
+import zio.test.TestAspect._
 
 import mdbKvs._
 import helpers._
@@ -19,7 +20,22 @@ object mdbKvsSpec extends DefaultRunnableSpec {
         payloadOutVbb   <-  mdbKvs.get(payLoadInKbb)
       } yield assert(dByteBufferToUtf8String(payloadOutVbb))(equalTo("value"))).provideCustomLayer(layers.layer)
     
+    ),
+
+    testM("put & get test2")(
+      (for {
+        mdbKvs          <-  accessDefaultMdbKvs
+        payLoadInKbb    =   stringToUtf8DByteBuffer("key2")
+        payLoadInVbb    =   stringToUtf8DByteBuffer("value2")
+        _               <-  mdbKvs.put(payLoadInKbb,payLoadInVbb)
+        payloadOutVbb   <-  mdbKvs.get(payLoadInKbb)
+      } yield assert(dByteBufferToUtf8String(payloadOutVbb))(equalTo("value2"))).provideCustomLayer(layers.layer)
+    
     )
-  )
+
+  ) @@ sequential
+  /* PS: this test would become non-deterministic and fail if you were to not use the sequential test aspect
+   * because then the same environment's opening second time could be interleaved randomly with its closing from the first time 
+   * */
 
 }
