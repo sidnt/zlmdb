5d1da87f7d4c937a348704d06ca329b008dda0d4
sidnt <11618157+sidnt@users.noreply.github.com>
2021-02-27 07:23:23 +0530

    some refactor to show structure, but

--- src/test/scala/zlmdb/a5-mdbKvsSpec.scala
+++ src/test/scala/zlmdb/a5-mdbKvsSpec.scala
@@ -1,5 +1,6 @@
 package zio.lmdb
 
+import zio._
 import zio.test._
 import zio.test.Assertion._
 import zio.test.TestAspect._
@@ -9,29 +10,48 @@ import helpers._
 
 object mdbKvsSpec extends DefaultRunnableSpec {
 
-  def spec = suite("mdb key value store spec")(
+  val getPutTest1: ZIO[MdbKvs, Throwable, TestResult] =
+    for {
+      mdbKvs          <-  accessDefaultMdbKvs
+      payLoadInKbb    =   stringToUtf8DByteBuffer("key1")
+      payLoadInVbb    =   stringToUtf8DByteBuffer("value1")
+      _               <-  mdbKvs.put(payLoadInKbb,payLoadInVbb)
+      payloadOutVbb   <-  mdbKvs.get(payLoadInKbb)
+    } yield assert(dByteBufferToUtf8String(payloadOutVbb))(equalTo("value1"))
+  
+  val getPutTestM1: ZSpec[MdbKvs, Throwable] = 
+    testM("put & get test1")(
+      getPutTest1
+    )
 
-    testM("put & get test")(
-      (for {
-        mdbKvs          <-  accessDefaultMdbKvs
-        payLoadInKbb    =   stringToUtf8DByteBuffer("key")
-        payLoadInVbb    =   stringToUtf8DByteBuffer("value")
-        _               <-  mdbKvs.put(payLoadInKbb,payLoadInVbb)
-        payloadOutVbb   <-  mdbKvs.get(payLoadInKbb)
-      } yield assert(dByteBufferToUtf8String(payloadOutVbb))(equalTo("value"))).provideCustomLayer(layers.layer)
-    
-    ),
+  val getPutTestM1P: ZSpec[ZEnv, Throwable] =
+    testM("put & get test1")(
+      getPutTest1.provideCustomLayer(layers.layer)
+    )
 
-    testM("put & get test2")(
-      (for {
+  val getPutTest2 = for {
         mdbKvs          <-  accessDefaultMdbKvs
         payLoadInKbb    =   stringToUtf8DByteBuffer("key2")
         payLoadInVbb    =   stringToUtf8DByteBuffer("value2")
         _               <-  mdbKvs.put(payLoadInKbb,payLoadInVbb)
         payloadOutVbb   <-  mdbKvs.get(payLoadInKbb)
-      } yield assert(dByteBufferToUtf8String(payloadOutVbb))(equalTo("value2"))).provideCustomLayer(layers.layer)
-    
-    )
+      } yield assert(dByteBufferToUtf8String(payloadOutVbb))(equalTo("value2"))
+
+  val getPutTestM2 = testM("put & get test1")(
+    getPutTest2
+  )
+
+  val getPutTestM2P = testM("put & get test2")(
+    getPutTest2.provideCustomLayer(layers.layer) 
+  )
+
+  val suite1 = suite("mdb key value store specs")(getPutTestM1, getPutTestM2)//.provideCustomLayer(layers.layer)
+
+  def spec = suite("mdb key value store specs")(
+
+    getPutTestM1P,
+
+    getPutTestM2P
 
   ) @@ sequential
   /* PS: this test would become non-deterministic and fail if you were to not use the sequential test aspect
